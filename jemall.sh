c8cf2882c1bb4df03398d70cecb0aec33e5951f9
Jay Paek <orange.ninjay@gmail.com>
2024-03-27 02:25:19 -0700

    new evals

--- jemall.sh
+++ jemall.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python jem/jemdoc.py index.jemdoc courses.jemdoc experiences.jemdoc projects.jemdoc publications.jemdoc 
+python jem/jemdoc.py index.jemdoc courses.jemdoc experience.jemdoc projects.jemdoc publications.jemdoc 
